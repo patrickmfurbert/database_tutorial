df36ee4905c7b6d7384feea212153082551344a9
Patrick Furbert <patrickmfurbert@gmail.com>
2023-03-03 22:17:42 -0500

    refactored db pool connection

--- Back/models/account.model.js
+++ Back/models/account.model.js
@@ -1,14 +1,4 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_URL,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASS,
-    port: process.env.DATABASE_PORT,
-});
+const pool = require('../config/db');
 
 //Define model for accounts table
 class Account {
